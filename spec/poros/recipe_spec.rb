1faa374576b96fdf3d3efa450ce93081ca01e712
Emily Port <emilylport@gmail.com>
2023-01-14 16:58:21 -0700

    recipe poros spec

---
+++ spec/poros/recipe_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :poros do 
+  describe 'it returns recipe objects', :vcr do 
+    it 'response has defined attributes' do 
+      recipes = RecipesFacade.recipes("Thailand")
+      expect(recipes).to be_an Array 
+      recipe = recipes.first 
+
+
+      expect(recipe).to be_an_instance_of Recipe
+      expect(recipe.country).to be_a String 
+      expect(recipe.title).to be_a String 
+      expect(recipe.url).to be_a String 
+    end
+  end
+end
