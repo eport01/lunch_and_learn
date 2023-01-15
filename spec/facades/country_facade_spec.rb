4429101d4465bf1769566571415864b65738b2ff
Emily Port <emilylport@gmail.com>
2023-01-15 14:09:57 -0700

    country facade spec

---
+++ spec/facades/country_facade_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper' 
+
+RSpec.describe CountryFacade do 
+  it 'creates an array of objects', :vcr do 
+    countries = CountryFacade.country 
+
+    expect(countries).to be_an Array 
+    expect(countries[0]).to be_an_instance_of(Country)
+
+    expect(countries[0].name).to eq("Grenada")
+
+  end
+end
