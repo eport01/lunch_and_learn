e4186a7b4f35d10261a920074b09b59dfec908ac
Emily Port <emilylport@gmail.com>
2023-01-15 14:12:18 -0700

    country poros spec

---
+++ spec/poros/country_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Country, type: :poros do 
+  describe 'it returns country objects', :vcr do 
+    it 'responses have name attributes' do 
+      countries = CountryFacade.country
+      expect(countries).to be_an Array 
+      country = countries.first 
+      expect(country).to be_an_instance_of Country
+      expect(country.name).to be_a String 
+      expect(country.name).to eq("Grenada")
+      expect(country.name).to_not eq("France")
+
+
+    end
+  end
+end
