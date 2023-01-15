a979f1de026e24e418afe462b7fc61889c3c7749
Emily Port <emilylport@gmail.com>
2023-01-15 16:52:56 -0700

    udpate testing to 100%

--- app/facades/recipes_facade.rb
+++ app/facades/recipes_facade.rb
@@ -1,12 +1,12 @@
 class RecipesFacade 
   def self.recipes(country)
     recipes = RecipesService.recipes_for_country(country)[:hits]
-    if recipes != [] && recipes.count >= 1
+    if recipes != [] && recipes.count > 1
       recipes.map do |recipe|
         Recipe.new(recipe[:recipe], country)
       end
     elsif recipes != [] && recipes.count == 1
-      Recipe.new(recipes[:recipe], country)
+      Recipe.new(recipes[0][:recipe], country)
     else
       []
     end
--- spec/facades/recipes_facade_spec.rb
+++ spec/facades/recipes_facade_spec.rb
@@ -14,18 +14,23 @@ RSpec.describe RecipesFacade do
 
   it 'creates an array of objects even if there is one object', :vcr do 
     recipes = RecipesFacade.recipes("Papua New Guinea")
-    expect(recipes).to be_an Array 
-    expect(recipes[0]).to be_an_instance_of(Recipe)
 
-    expect(recipes[0].country).to eq("Papua New Guinea")
-    expect(recipes[0].country).to_not eq("Thailand")
+    expect(recipes).to be_an_instance_of(Recipe)
 
+    expect(recipes.country).to eq("Papua New Guinea")
+    expect(recipes.country).to_not eq("Thailand")
+
+    expect(recipes.title).to eq("Papua New Guinea Baked Sweet Potatoes")
+    expect(recipes.url).to eq("http://www.food.com/recipe/papua-new-guinea-baked-sweet-potatoes-457887")
     
   end
 
   it 'returns an empty array if country has no recipes', :vcr do 
     recipes = RecipesFacade.recipes("Equatorial Guinea")
+
     expect(recipes).to be_an Array 
+    expect(recipes[0]).to_not be_an_instance_of(Recipe)
+
     expect(recipes).to eq([])
 
     
