7efa397da96dfe879e5a36df7cab15ab6defa6b3
Emily Port <emilylport@gmail.com>
2023-01-17 09:12:25 -0700

    update video and image specs

--- spec/poros/image_spec.rb
+++ spec/poros/image_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe Image, type: :poros do 
   describe 'it returns image objects', :vcr do 
     it 'response has defined attributes' do 
+      images = ImageFacade.images("Laos")
+      expect(images). to be_an Array 
 
+      expect(images[0]).to be_an_instance_of Image 
+      expect(images[0].alt_tag).to be_a String 
+      expect(images[0].url).to be_a String 
 
     end
   end
--- spec/poros/video_spec.rb
+++ spec/poros/video_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe Video, type: :poros do 
   describe 'it returns video objects' do 
     it 'response has defined attributes' do 
-
+      video = VideoFacade.country_videos("Laos")
+      
+      expect(video).to be_an_instance_of Video 
+      expect(video.title).to be_a String 
+      expect(video.youtube_video_id).to be_a String 
 
     end
   end
