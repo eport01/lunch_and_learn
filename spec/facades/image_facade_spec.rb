fd427029b02b536e43bad6dbaf18b12af3d17896
Emily Port <emilylport@gmail.com>
2023-01-16 18:45:28 -0700

    image facade spec

---
+++ spec/facades/image_facade_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper' 
+
+RSpec.describe ImageFacade do 
+  it 'creates max 10 image objects based on a country', :vcr do 
+    images = ImageFacade.images("Laos")
+    expect(images).to be_an Array 
+
+    expect(images.count).to be <= 10 
+
+    expect(images[0]).to be_an_instance_of Image 
+
+    expect(images[0].alt_tag).to eq("green trees near body of water during daytime")
+    expect(images[0].url).to eq("https://images.unsplash.com/photo-1606064195579-a48c728cec35?ixid=MnwzOTk3ODB8MHwxfHNlYXJjaHwxfHxMYW9zfGVufDB8fHx8MTY3MzkxODI5Nw&ixlib=rb-4.0.3")
+  
+  end
+end
---
+++ spec/facades/video_facade_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper' 
+
+RSpec.describe VideoFacade do 
+  it 'creates a video object for videos based on a country', :vcr do 
+    video = VideoFacade.country_videos("Laos")
+
+    expect(video).to be_an_instance_of Video  
+
+    expect(video.title).to eq("A Super Quick History of Laos")
+    expect(video.youtube_video_id).to eq("uw8hjVqxMXw")
+  end
+end
