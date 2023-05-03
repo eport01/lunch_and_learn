955fa8ab828aae0ad455cf119fb4544384e08dbd
Emily Port <emilylport@gmail.com>
2023-05-03 13:08:38 -0600

    all tests pass

--- spec/requests/api/v1/recipes_spec.rb
+++ spec/requests/api/v1/recipes_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'return recipes for a country' do 
+  before :each do 
+    user = {
+      "name": "Emily Port",
+      "email": "emily@mod3_is_almost_complete.com", 
+      "password": "1234"
+    }
+    expect(user[:api_key]).to eq(nil)
+
+    post "/api/v1/users", headers: {'CONTENT_TYPE' => 'application/json'}, params: JSON.generate(user)
+
+    @saved_user = User.last
+  end
   it 'returns recipes when a user inputs a recipe as params', :vcr do 
     country = "Peru"
-    get "/api/v1/recipes?country=#{country}"
+    get "/api/v1/recipes?country=#{country}&api_key=#{@saved_user.api_key}"
     expect(response).to be_successful
 
     recipes = JSON.parse(response.body, symbolize_names: true)[:data]
@@ -30,7 +42,7 @@ RSpec.describe 'return recipes for a country' do
 
   it 'returns recipes for a random country when no country was input' do 
 
-    get "/api/v1/recipes?country="
+    get "/api/v1/recipes?country=&api_key=#{@saved_user.api_key}"
     
     expect(response).to be_successful
 
@@ -58,7 +70,7 @@ RSpec.describe 'return recipes for a country' do
   end
 
   it 'returns an empty array if country entered is invalid or no recipes exist', :vcr do 
-    get "/api/v1/recipes?country=dog"
+    get "/api/v1/recipes?country=dog&api_key=#{@saved_user.api_key}"
 
     expect(response).to be_successful
 
